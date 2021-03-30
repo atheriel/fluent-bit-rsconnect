8331d4b9cdb2697c0852766bb529bbec100fc260
Aaron Jacobs <aaron.jacobs@crescendotechnology.com>
2021-03-30 20:46:49 +0000

    License under the LGPL 2.1.

--- filter_rsconnect/rsconnect.c
+++ filter_rsconnect/rsconnect.c
@@ -1,5 +1,22 @@
 /* -*- mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
 
+/*
+ * Copyright 2021 Aaron Jacobs
+ *
+ * This plugin is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this plugin; if not, see https://www.gnu.org/licenses/.
+ */
+
 #include <fluent-bit/flb_hash.h>
 #include <fluent-bit/flb_info.h>
 #include <fluent-bit/flb_filter.h>
